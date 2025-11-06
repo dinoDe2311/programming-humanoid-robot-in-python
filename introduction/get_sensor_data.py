5356a169d692546badcc06a895dd1b61b3a310f9
Dino <dino.cataldo@gmail.com>
2025-11-06 23:30:07 +0100

    first assignment

--- introduction/get_sensor_data.py
+++ introduction/get_sensor_data.py
@@ -10,6 +10,7 @@ In this exercise you need to know how to get sensor data.
 # add PYTHONPATH
 import os
 import sys
+
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'software_installation'))
 
 from spark_agent import SparkAgent
@@ -17,14 +18,8 @@ from spark_agent import SparkAgent
 
 class MyAgent(SparkAgent):
     def think(self, perception):
-        angle = 0
-        temperature = 0
-        # YOUR CODE HERE
-        # get angle and temperature to current data of joint HeadYaw
+        angle = perception.joint.get('HeadYaw', 0)
+        temperature = perception.joint_temperature.get('HeadYaw', 0)
 
         print('HeadYaw angle: ' + str(angle) + ' temperature: ' + str(temperature))
         return super(MyAgent, self).think(perception)
-
-if '__main__' == __name__:
-    agent = MyAgent()
-    agent.run()
--- introduction/set_joint_commands.py
+++ introduction/set_joint_commands.py
@@ -12,6 +12,7 @@ In this exercise you need to know how to set joint commands.
 # add PYTHONPATH
 import os
 import sys
+
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'software_installation'))
 
 from spark_agent import SparkAgent
@@ -20,9 +21,11 @@ from spark_agent import SparkAgent
 class MyAgent(SparkAgent):
     def think(self, perception):
         action = super(MyAgent, self).think(perception)
-        # YOUR CODE HERE
+        action.stiffness['LShoulderPitch'] = 0.0
+        action.speed['HeadYaw'] = 0.1
 
         return action
+
 
 if '__main__' == __name__:
     agent = MyAgent()
